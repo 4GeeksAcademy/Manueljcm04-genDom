0f4ee1f7071b4680edce291998e2c41b8a7e36d3
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-08 21:30:45 +0000

    genDom

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomIndexFunction = length => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+
+  for (let i in (pronoun, adj, noun)) {
+    const randomIndex1 = randomIndexFunction(pronoun.length);
+    const randomIndex2 = randomIndexFunction(adj.length);
+    const randomIndex3 = randomIndexFunction(noun.length);
+    const randomName1 = pronoun[randomIndex1];
+    const randomName2 = adj[randomIndex2];
+    const randomName3 = noun[randomIndex3];
+
+    console.log(randomName1 + randomName2 + randomName3 + ".com");
+  }
 };
