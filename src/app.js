3b4c4827d5fd4e1d12da9052d2d0deee234e3042
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-18 08:58:53 +0000

    GenDon

--- src/app.js
+++ src/app.js
@@ -6,25 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const randomIndexFunction = length => {
-    return Math.floor(Math.random() * length);
-  };
-
   const pronoun = ["the", "our"];
   const adj = ["great", "big"];
   const noun = ["jogger", "racoon"];
 
-  for (let i in (pronoun, adj, noun)) {
-    const randomIndex1 = randomIndexFunction(pronoun.length);
-    const randomIndex2 = randomIndexFunction(adj.length);
-    const randomIndex3 = randomIndexFunction(noun.length);
-    const randomName1 = pronoun[randomIndex1];
-    const randomName2 = adj[randomIndex2];
-    const randomName3 = noun[randomIndex3];
-
-    const randomIndex = randomName1 + randomName2 + randomName3 + ".com";
-    document.getElementById("name").innerHTML = randomIndex;
-
-    console.log(randomName1 + randomName2 + randomName3 + ".com");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
   }
 };
